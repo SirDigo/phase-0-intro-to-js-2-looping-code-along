9f4f81ab771962b08096140b63712fcb6d526b59
Caleb Rodriguez <sirdigo6@gmail.com>
2021-11-09 14:34:54 -0500

    Completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    const messages =[];
+    for (let i = 0; i < name.length; i++){
+        messages.splice(i,0, `Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(int) {
+    let number = int;
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+    return number;
+}
+
